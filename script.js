af39da55182b792972aad1e709adfd36b0661708
Paul Kanas <paulkanas@gmail.com>
2022-10-08 15:52:24 +1030

    Added GUI to RPS game

--- script.js
+++ script.js
@@ -1,19 +1,101 @@
 
-const button = document.createElement('button');
-const button2 = document.createElement('button');
-const button3 = document.createElement('button');
-
-button.textContent = "Rock";
-button2.textContent = "Paper";
-button3.textContent = "Scissors";
-
-document.body.appendChild(button);
-document.body.appendChild(button2);
-document.body.appendChild(button3);
-
+const scores = document.querySelector('#scores');
+const para = document.createElement('p');
+const para2 = document.createElement('p');
+const para3 = document.createElement('p');
+const youChose = document.createElement('p');
+const theyChose = document.createElement('p');
+const resultPara = document.createElement ('p');
 
 let yourScore = Number(0);
 let computerScore = Number(0);
+
+const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      playRound(button.textContent, getComputerChoice())
+      para2.textContent = "The computer's score is " + computerScore + ".";
+      para.textContent = "Your score is " + yourScore + ".";
+
+      if (yourScore >= 5) {
+        para3.textContent = "You scored 5! You win the series!";
+        setGameOver();
+      } else if (computerScore >= 5) {
+        para3.textContent = "The computer scored 5! The computer wins the series!";
+        setGameOver();
+      } else {
+        para3.textContent = "First to 5 wins!";
+      }
+    });
+  });
+
+function disableBtn() {
+    var elems = document.getElementsByClassName('button');
+    for (var i=0;i<elems.length;i+=1){
+      elems[i].style.display = 'none';
+    }
+}
+
+function enableBtn() {
+    var elems = document.getElementsByClassName('button');
+    for (var i=0;i<elems.length;i+=1){
+      elems[i].style.display = 'initial';
+    }
+}
+
+
+para3.textContent = "First to 5 wins!";
+youChose.textContent = "You chose: ";
+theyChose.textContent = "The computer chose: ";
+resultPara.textContent = "And the winner is...";
+
+scores.appendChild(youChose);
+scores.appendChild(theyChose);
+scores.appendChild(resultPara);
+scores.appendChild(para);
+scores.appendChild(para2);
+scores.appendChild(para3);
+
+function setGameOver() {
+    disableBtn()
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    yourScore = 0;
+    computerScore = 0;
+    resetParas();
+    enableBtn();
+    resetButton.style.display = 'none';
+}
+
+function resetParas() {
+    var allParas = document.querySelectorAll('p');
+    for (var i=0;i<allParas.length;i+=1){
+        allParas[i].textContent = "";
+      }
+}
+
+
+// buttons.forEach(function(currentBtn){
+//   currentBtn.addEventListener('click', playRound)
+// })
+
+//   button.addEventListener('click', () => {
+//     playRound(button.textContent, getComputerChoice())
+// });
+
+// button2.addEventListener('click', () => {
+//     playRound(button2.textContent, getComputerChoice())
+// });
+
+
+
+
+
 const values = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
@@ -30,31 +112,33 @@ function getplayerOneChoice() {
 }
 
 function playRound(playerOne, playerTwo) {
-    console.log("You chose: " + playerOne);
-    console.log("The computer chose: " + playerTwo);
+    youChose.textContent = "You chose: " + playerOne + ".";
+    theyChose.textContent = "The computer chose: " + playerTwo + ".";
+    para.textContent = "Your score is " + yourScore + ".";
+    para2.textContent = "The computer's score is " + computerScore + ".";
 
     if (playerOne === playerTwo) {
-        return "It's a tie. No one wins"
+        resultPara.textContent = "It's a tie. No one wins."
     } else if (playerOne === "Rock" && playerTwo === "Paper") {
         computerScore++;
-        return "Rock loses to paper. You lose!";
+        resultPara.textContent = "Rock loses to paper. You lose!";
     } else if (playerOne === "Rock" && playerTwo === "Scissors") {
         yourScore++;
-        return "Rock beats scissors. You win!";
+        resultPara.textContent = "Rock beats scissors. You win!";
     } else if (playerOne === "Scissors" && playerTwo === "Paper") {
         yourScore++;
-        return "Scissors beats paper. You win!";
+        resultPara.textContent = "Scissors beats paper. You win!";
     } else if (playerOne === "Scissors" && playerTwo === "Rock") {
         computerScore++;
-        return "Scissors loses to rock. You lose!";
+        resultPara.textContent = "Scissors loses to rock. You lose!";
     } else if (playerOne === "Paper" && playerTwo === "Scissors") {
         computerScore++;
-        return "Paper loses to scissors. You lose!";
+        resultPara.textContent = "Paper loses to scissors. You lose!";
     } else if (playerOne === "Paper" && playerTwo === "Rock") {
         yourScore++;
-        return "Paper beats rock. You win!";
+        resultPara.textContent = "Paper beats rock. You win!";
     } else {
-        return "Please choose either 'rock', 'paper', or 'scissors'";
+        resultPara.textContent = "Please choose either 'rock', 'paper', or 'scissors'";
     }
 }
 
